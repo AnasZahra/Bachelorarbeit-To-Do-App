2a946148052b77c0f27601648e35ac5f074615bd
Anas Zahra <pib.anas.zahra@htwsaar.de>
2024-09-26 13:08:08 +0200

    Auth 0 implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,31 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FooterComponent } from './footer/footer.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     TodoListComponent,
     FooterComponent,
   ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(public auth: AuthService){{
+
+  }}
+
+  login() {
+    this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout({});
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-7yujk5octccad0s1.eu.auth0.com',
+      clientId: 'jwjx1mh8mODecUWqJlcRZesgE3Xey5xI',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+    ...appConfig.providers // Spread the existing appConfig providers if needed
+  ]
+})
   .catch((err) => console.error(err));
